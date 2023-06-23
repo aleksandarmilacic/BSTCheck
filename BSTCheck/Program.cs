0e2ad5f20518940c9e9768871329d983e35b6816
amilacic <aleksandar.milacic@gmail.com>
2023-06-24 00:17:19 +0200

    Add project files.

---
+++ BSTCheck/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace BSTCheck
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        private TreeNode prev = null;
+
+        public bool IsValidBST(TreeNode root)
+        {
+            // Inorder traversal
+            if (root != null)
+            {
+                if (!IsValidBST(root.left))
+                {
+                    return false;
+                }
+
+                if (prev != null && root.val <= prev.val)
+                {
+                    return false;
+                }
+
+                prev = root;
+                return IsValidBST(root.right);
+            }
+
+            return true;
+        }
+
+        public void PrintTree(TreeNode node, string indent = "")
+        {
+            if (node != null)
+            {
+                Console.WriteLine(indent + node.val);
+                indent += "  ";  // increase the indent as we go deeper
+                PrintTree(node.left, indent);
+                PrintTree(node.right, indent);
+            }
+        }
+
+    }
+
+    internal class Program
+    {
+        public static void Main()
+        {
+            Solution solution = new Solution();
+            TreeNode node = new TreeNode(2);
+            node.left = new TreeNode(1);
+            node.right = new TreeNode(3);
+            bool result = solution.IsValidBST(node);
+            Console.WriteLine(result);  // It should print: True
+
+            solution.PrintTree(node);   // This will print the tree
+        }
+    }
+}
